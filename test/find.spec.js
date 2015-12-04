f498e5dda145a97007d29052bd5f7d3edecd6f1a
pleasurazy <pleasurazy@gmail.com>
2015-12-04 12:49:39 +0800

    test could test for many urls

--- test/find.spec.js
+++ test/find.spec.js
@@ -1,9 +1,7 @@
-import _ from 'lodash'
 import {expect} from 'chai'
 import crawler from '../src/index'
 import isISOString from 'isostring'
 import isUrl from 'is-url'
-
 
 describe('find()', function() {
 
@@ -11,25 +9,31 @@ describe('find()', function() {
 
   it('Basic usage', async () => {
 
-    let article = await crawler.find({
-      url: 'http://money9992.pixnet.net/blog/post/427735169'
-    })
+    let urls = [
+      'http://money9992.pixnet.net/blog/post/427735169',
+      'http://naichennai.pixnet.net/blog/post/59979496',
+    ]
 
-    expect(article).to.be.an('object')
-    expect(article).to.include.keys('title', 'url', 'published', 'body', 'images', 'address')
+    for (let url of urls) {
+      let article = await crawler.find({ url })
 
-    expect(article.title).to.be.a('string', '必須要有 title')
+      expect(article).to.be.an('object')
+      expect(article).to.include.keys('title', 'url', 'published', 'body', 'images', 'address')
 
-    expect(isUrl(article.url)).to.equal(true, '必須要有 url :string<URL>')
+      expect(article.title).to.be.a('string', '必須要有 title')
 
-    expect(isISOString(article.published)).to.equal(true, '必須要有 published :string<ISO8601>')
+      expect(isUrl(article.url)).to.equal(true, '必須要有 url :string<URL>')
 
-    expect(article.body).to.be.a('string', '必須要有 body :string<HTMLString>')
+      expect(isISOString(article.published)).to.equal(true, '必須要有 published :string<ISO8601>')
 
-    expect(article.images).to.be.an('array')
-    _.each(article.images, (img) => expect(isUrl(img)).to.equal(true, 'images 裡的物件必須是 url :string<URL>'))
+      expect(article.body).to.be.a('string', '必須要有 body :string<HTMLString>')
 
-    expect(article.address).to.be.an('array')
-    _.each(article.address, (addr) => expect(addr).to.be.a('string', 'images 裡的物件必須是 地址 :string'))
+      expect(article.images).to.be.an('array')
+      for (let img of article.images) { expect(isUrl(img)).to.equal(true, 'images 裡的物件必須是 url :string<URL>') }
+
+      expect(article.address).to.be.an('array')
+      for (let addr of article.address) { expect(addr).to.be.a('string', 'images 裡的物件必須是 地址 :string') }
+    }
+
   })
 })
